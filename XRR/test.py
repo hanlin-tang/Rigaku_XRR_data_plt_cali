cad19a92ef860a6d2df02a015ebe495103a72d43
hanlin-tang <hl.tang0917@gmail.com>
2023-12-04 13:35:14 -0500

    upload files

---
+++ XRR/test.py
@@ -0,0 +1,40 @@
+# -*-coding:utf-8-*-
+# Import math Library
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+# def transform_arrays(A, B, x):
+#     for i, a in enumerate(A):
+#         if a < x:
+#             B[i] = B[i] / math.sin(math.radians(a))
+#             print(math.sin(math.radians(a)))
+#     return B
+# #
+# # file_path = 'C:\\Users\\12243\\Desktop\\CWO_XRD\\XRR\\I-1 xrr fit 29 nm 6.5g 0.txt'
+# #
+# data = np.loadtxt('I-1 xrr fit 29 nm 6.5g 0.txt')
+#
+#
+# x = data[:, 0]
+# y = data[:, 1]
+# plt.plot(x, y)
+# # plt.yscale('log')
+# # plt.show()
+# # # Read data from the text file
+# #
+#
+#
+#
+#
+#
+# def normall(A):
+#     I0 = np.max(A)
+#     for i, a in enumerate(A):
+#
+#         A[i] = A[i] / I0
+#
+#     return A
+# print(normall([10,9,8,7]))
+a = 'iiii7'
+b = list(a)
+print (b[-1])
